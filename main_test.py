a908da6a69f22dbaeb1f856ecd05bf88057409c0
raquena <raul.quesadanavarro@raquenaengineering.com>
2019-06-05 16:51:08 +0200

    Start preparing test for serial autodetect.

--- main_test.py
+++ main_test.py
@@ -5,18 +5,51 @@
 from fy6800 import *
 
 
+port = "COM8"			# fixed com port for testing purposes 
+
+
 
 func_gen = fy6800()
 
 
-port = "COM8"			# fixed com port for testing purposes 
-
+# get/set_serialport #
 func_gen.set_serialport(port)
-
 requested_port = (func_gen.get_serialport())
+print("Serial port of the functon generator: " + requested_port)
 
-print(requested_port)
 
+#autodetect serial port #
 #port = func_gen.autodetect_port()
+
+
+# set/get wave type #
+
+channel = 0
+func_gen.set_wave(channel,sine)
+req_wave = func_gen.get_wave(channel)
+print("Wave on port " + str(channel) + ": " + req_wave)
+
+channel = 1
+func_gen.set_wave(channel,triangle)
+req_wave = func_gen.get_wave(channel)
+print("Wave on port " + str(channel) + ": " + req_wave)
+
+# modify get/set to return the name of the wave, instead of its number.  (tuples ???)
+
+#set/get amplitude#
+
+channel = 0
+ampl = 0.01
+func_gen.set_ampl(channel,ampl)
+req_ampl = func_gen.get_ampl(channel)
+print("Amplitude on port " + str(channel) + ": " + str(req_ampl))
+
+channel = 1
+ampl = 1
+func_gen.set_ampl(channel,ampl)
+req_ampl = func_gen.get_ampl(channel)
+print("Amplitude on port " + str(channel) + ": " + str(req_ampl))
+
+
 
 
